823b08e25cd6d8392678f82098d0c939e7a713ea
zimok <simonecarella@gmail.com>
2014-01-23 16:01:29 +0100

    slightly edited demo

--- demo.py
+++ demo.py
@@ -1,34 +1,25 @@
 from rslvr.resolvers.deezer import DeezerResolver
 from rslvr.resolvers.spotify import SpotifyResolver
-from rslvr.resolvers.rdio import RdioResolver
-from rslvr.resolvers.lastfm import LastfmResolver
 from rslvr.resolvers.musicbrainz import MusicBrainzResolver
 from rslvr.resolvers.nokiamusic import NokiaMusicResolver
 
 from rslvr.resolver import ResolverService
 
 resolvers = [
-	# DeezerResolver,
-	# SpotifyResolver,
-	# LastfmResolver,
+	DeezerResolver,
+	SpotifyResolver,
 	NokiaMusicResolver
 ]
 
 mr = ResolverService(resolvers)
 
 tracks = [
-	('how i could just kill a man', 'cypress hill'),
-	('cypress hill how i could just kill a man', 'cypress hill', 'how i could just kill a man'),
-	('harmony dumbo gets mad', 'dumbo gets mad', 'harmony'),
-	('depeche mode enjoy the silence', 'depeche mode', 'enjoy the silence'),
-	('the end summer camp', 'summer camp', 'the end'),
-	('justice stress', 'justice', 'stress'),
-	('1901 phoenix', 'phoenix', '1901'),
+	('cypress hill how i could just kill a man', 'cypress hill'),
+	('1901 phoenix', 'phoenix'),
 ]
 
 for i, t in enumerate(tracks):
 	r = mr.resolve(t[0], fields=['id', 'name', 'artist', 'artist_id'], meta={})
-	#r = mr.resolve(t[0], fields=['id', 'name', 'artist', 'artist_id'])
 	
 	print r.json(sort_keys=True, indent=4)
 
